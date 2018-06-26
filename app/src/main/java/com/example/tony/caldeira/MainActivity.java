5dbc408b4fdd656abe2fcf91a3794883733a575e
asergio <asergio@ua.pt>
2018-06-26 11:32:09 +0100

    Added event listener for toggle button
    > Opacity of boiler image changes when toggle is checked/unchecked

--- app/src/main/java/com/example/tony/caldeira/MainActivity.java
+++ app/src/main/java/com/example/tony/caldeira/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.tony.caldeira;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ToggleButton;
+import android.widget.CompoundButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected ToggleButton toggle;  // ON/OFF Boiler ToggleButton Object
+    protected ImageView caldeira;  // Boiler Image Object
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toggle = findViewById(R.id.toggleButton);
+        caldeira = findViewById(R.id.caldeiraImage);
+
+        // When toggleButton is clicked
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)  turnCaldeiraOff();
+                else            turnCaldeiraOn();
+            }
+        });
+
+
     }
+
+    protected void turnCaldeiraOn(){
+        caldeira.setImageAlpha(255);
+    }
+
+    protected void turnCaldeiraOff(){
+        caldeira.setImageAlpha(100);
+    }
+
 }
